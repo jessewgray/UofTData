711e8a4488671d89bf314abe57aaaba75b152a16
Jesse Gray <jwgray2@gmail.com>
2018-10-28 22:13:15 -0400

    update

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -19,7 +19,8 @@ with open(dataPath, "r", newline = "", encoding = "utf-8") as csvFile:
     print(f"Total Months: {index}")
     print(f"Total: {combinedMonthsTotal}")
     
-#with open(dataPath, "w", newline="", encoding = "utf-8") as csvResults:
-    #csvWriter = csv.writer(csvResults, delimiter=",")
-    
-    
+with open("resources/results.csv", "w", newline="", encoding = "utf-8") as csvResultsFile:
+    csvWriter = csv.writer(csvResultsFile, delimiter=",")
+    csvWriter.writerow("Financial Analysis")
+    csvWriter.writerow(f"Total Months: {index}")
+    csvWriter.writerow(f"Total: ${combinedMonthsTotal}")
